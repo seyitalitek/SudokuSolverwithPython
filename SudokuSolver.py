732726087df092ac6b5702a872bfe8244d167497
ali <seyitalitek@gmail.com>
2020-07-27 13:44:18 +0200

    changes in names of classes and fucntions

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,58 +1,58 @@
 class Table:
     def __init__(self):
-        self.dizi = list()
+        self.grid = list()
         for i in range(9):
-            self.dizi.append([])
+            self.grid.append([])
             for j in range(9):
-                self.dizi[i].append(Eleman(i, j, 0))
+                self.grid[i].append(Element(i, j, 0))
 
         self.numberOfUnsolved = 0
 
-        for i in self.dizi:
+        for i in self.grid:
             for e in i:
                 if e.value == 0:
                     self.numberOfUnsolved += 1
 
     def updateUnsolved(self):
         self.numberOfUnsolved = 0
-        for i in self.dizi:
+        for i in self.grid:
             for e in i:
                 if e.value == 0:
                     self.numberOfUnsolved += 1
 
-    def setEleman(self, row, column, neu):
-        self.dizi[row][column].value = neu
+    def setElement(self, row, column, neu):
+        self.grid[row][column].value = neu
 
-    def getunsolved(self):
+    def getUnsolved(self):
         unsolved = list()
-        for row in self.dizi:
+        for row in self.grid:
             for e in row:
                 if e.value == 0:
-                    e.buildpossibles(self)
+                    e.buildPossibles(self)
                     unsolved.append(e)
         unsolved.sort(key=lambda x: len(x.possibles))
         return unsolved
 
     def __str__(self):
-        cikti = "\n\n    | | | | | | | | |  -  (j)\n  - "
+        output = "\n\n    | | | | | | | | |  -  (j)\n  - "
         for i in range(9):
             c = 0
-            for e in self.dizi[i]:
+            for e in self.grid[i]:
                 if c % 3 == 2 and c != 8:
-                    cikti += str(e.value if e.value != 0 else " ") + "|"
+                    output += str(e.value if e.value != 0 else " ") + "|"
                 else:
-                    cikti += str(e.value if e.value != 0 else " ") + " "
+                    output += str(e.value if e.value != 0 else " ") + " "
                 c += 1
             if i % 3 == 2 and i != 8:
-                cikti += "\n    -----------------\n  - "
+                output += "\n    -----------------\n  - "
             elif i != 8:
-                cikti += "\n  - "
+                output += "\n  - "
             else:
-                cikti += "\n\n  |\n\n (i)"
-        return cikti
+                output += "\n\n  |\n\n (i)"
+        return output
 
 
-class Eleman:
+class Element:
     def __init__(self, i, j, value):
         self.digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         self.row = i
@@ -67,15 +67,15 @@ class Eleman:
     def __repr__(self):
         return str(self.value if self.value != 0 else " ")
 
-    def buildpossibles(self, tablo):
+    def buildPossibles(self, tablo):
         rowothers = list()
         columnothers = list()
         smallsqothers = list()
-        for e in tablo.dizi[self.row]:
+        for e in tablo.grid[self.row]:
             rowothers.append(e.value)
         for i in range(9):
-            columnothers.append(tablo.dizi[i][self.column].value)
-        for row in tablo.dizi:
+            columnothers.append(tablo.grid[i][self.column].value)
+        for row in tablo.grid:
             for e in row:
                 if e.smallsq == self.smallsq:
                     smallsqothers.append(e.value)
@@ -90,7 +90,7 @@ class Eleman:
 def solve(tablo):
     tablo.updateUnsolved()
 
-    lst = tablo.getunsolved()
+    lst = tablo.getUnsolved()
 
     if len(lst) == 0:
         return tablo
@@ -100,10 +100,10 @@ def solve(tablo):
         for possible in lst[0].possibles:
             x = lst[0].row
             y = lst[0].column
-            tablo.dizi[x][y].value = possible
+            tablo.grid[x][y].value = possible
 
             if solve(tablo) is None:
-                tablo.dizi[x][y].value = 0
+                tablo.grid[x][y].value = 0
 
             else:
 
@@ -113,27 +113,27 @@ def solve(tablo):
 
 ornek = Table()
 
-ornek.setEleman(0, 0, 8)
-ornek.setEleman(1, 2, 3)
-ornek.setEleman(2, 1, 7)
-ornek.setEleman(1, 3, 6)
-ornek.setEleman(2, 4, 9)
-ornek.setEleman(2, 6, 2)
-ornek.setEleman(3, 1, 5)
-ornek.setEleman(3, 5, 7)
-ornek.setEleman(4, 4, 4)
-ornek.setEleman(4, 5, 5)
-ornek.setEleman(4, 6, 7)
-ornek.setEleman(5, 3, 1)
-ornek.setEleman(5, 7, 3)
-ornek.setEleman(6, 2, 1)
-ornek.setEleman(6, 8, 8)
-ornek.setEleman(6, 7, 6)
-ornek.setEleman(7, 2, 8)
-ornek.setEleman(7, 3, 5)
-ornek.setEleman(7, 7, 1)
-ornek.setEleman(8, 1, 9)
-ornek.setEleman(8, 6, 4)
+ornek.setElement(0, 0, 8)
+ornek.setElement(1, 2, 3)
+ornek.setElement(2, 1, 7)
+ornek.setElement(1, 3, 6)
+ornek.setElement(2, 4, 9)
+ornek.setElement(2, 6, 2)
+ornek.setElement(3, 1, 5)
+ornek.setElement(3, 5, 7)
+ornek.setElement(4, 4, 4)
+ornek.setElement(4, 5, 5)
+ornek.setElement(4, 6, 7)
+ornek.setElement(5, 3, 1)
+ornek.setElement(5, 7, 3)
+ornek.setElement(6, 2, 1)
+ornek.setElement(6, 8, 8)
+ornek.setElement(6, 7, 6)
+ornek.setElement(7, 2, 8)
+ornek.setElement(7, 3, 5)
+ornek.setElement(7, 7, 1)
+ornek.setElement(8, 1, 9)
+ornek.setElement(8, 6, 4)
 ornek.updateUnsolved()
 print(ornek)
 
